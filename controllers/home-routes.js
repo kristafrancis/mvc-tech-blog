dc816fbe0304ba10552ae9c9485013246222419e
kristafrancis721 <kristafrancis08@yahoo.com>
2022-09-14 20:21:08 -0400

    fixed errors in routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,6 @@ router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
       'id',
-      'post_text',
       'title',
       'created_at'
     ],
@@ -48,7 +47,6 @@ router.get('/post/:id', (req, res) => {
     },
     attributes: [
       'id',
-      'post_text',
       'title',
       'created_at'
     ],
